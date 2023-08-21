99aaae0a435bfc0f10a0aa7d1f6c3b1a588525a3
satybal <satybal_tim@mail.ru>
2023-08-21 13:18:11 +0300

    before cntrs and iter (single forward)

--- 06_containers/containers.cpp
+++ 06_containers/containers.cpp
@@ -5,11 +5,29 @@
 
 int main() {
   //Sequence::test_container();
-  Container con;
+  Container<int> con;
   con.push_back(5);
   con.push_back(15);
   con.push_back(25);
+  con.push_back(35);
   con.print();
+
+  // con.erase(0);
+  // con.print();
+
+  // con.erase(1);
+  // con.print();
+
+  // con.erase(1);
+  // con.print();
+
+  // con.insert(0, 10);
+  // con.insert(0, 11);
+  // con.insert(1, 13);
+  // con.print();
+
+  // std::cout << con.size() << std::endl;
+  // std::cout << con[1] << std::endl;
 
   return 0;
 }
--- 06_containers/listContainer.hpp
+++ 06_containers/listContainer.hpp
@@ -1,19 +1,34 @@
 #include <iostream>
-
-//template <typename T>
+template <typename T>
 struct Node {
     Node() {}
 
     Node* next;
-    int value;
+    T value;
 };
 
+template <typename T>
 class Container {
 public:
     Container() {}
 
-    void print() {
-        Node *curr = head;
+    Node<T> *move_idx(size_t idx) const {
+        Node<T> *_node = head;
+
+        for (size_t i = 0; i < idx; ++i) {
+            _node = _node->next;
+        }
+
+        return _node;
+    }
+
+    T &operator[](size_t idx) {
+        Node<T> *value = move_idx(idx);
+        return value->value;
+    }
+
+    void print() const { // PRINT
+        Node<T> *curr = head;
 
         while (curr != nullptr) {
             std::cout << curr->value << " ";
@@ -24,8 +39,8 @@ public:
         std::cout << std::endl;
     }
     
-    void push_back(int value) {
-        Node *new_node = new Node{};
+    void push_back(T value) { //PUSH BACK
+        Node<T> *new_node = new Node<T>{};
 
         new_node->value = value;
         new_node->next = nullptr;
@@ -39,8 +54,44 @@ public:
         m_size++;
     }
 
+    void erase(size_t idx) { //ERASE
+        Node<T> *_cur = move_idx(idx);
+
+        m_size--;
+
+        if (_cur->next) {
+            if (idx == 0)
+                head = _cur->next;
+            else {
+                Node<T> *_prev = move_idx(idx - 1);
+                _prev->next = _cur->next;
+            }
+        } else {
+            Node<T> *_prev = move_idx(idx - 1);
+            _prev->next = nullptr;
+        }
+    }
+
+    void insert(size_t idx, T value) { //INSERT
+        Node<T> *_curr = move_idx(idx);
+
+        Node<T> *new_node = new Node<T>{};
+            new_node->next = _curr;
+            new_node->value = value;
+        if (idx == 0) {
+            head = new_node;
+        } else {
+            Node<T> *_prev = move_idx(idx - 1);
+            _prev->next = new_node;
+        }
+
+        m_size++;
+    }
+
+    size_t size() { return m_size; }
+
 private:
-    Node *tail;
-    Node *head;
+    Node<T> *tail;
+    Node<T> *head;
     size_t m_size = 0;
 };
